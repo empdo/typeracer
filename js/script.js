e8efa8685f32f4f7f80c25e844456a67c08f64bd
Emil Essung <emil.essung@live.se>
2020-10-13 22:52:01 +0200

    fixeds stuuf2

--- js/script.js
+++ js/script.js
@@ -1,71 +1,71 @@
 const input = document.querySelector(".input-field");
-input.value ="";
+input.value = "";
 
-var code = 'function positionIndex()'
+var code = "var hlTypedText = hastUtilToHtml({ type: 'root', children: light.value });";
 
-var light = lowlight.highlight('js', code)
-
-var html = hastUtilToHtml({type: 'root', children: light.value});
-console.log(light.value);
-document.write(html);
-
-validateWord()
 function validateWord() {
-    var textSaker = code.split(" ");
-    console.log(textSaker);
+  var textSaker = code.split(" ");
+  console.log(textSaker);
 }
 
 /**
  * @type {String[]}
  */
-var typedText = [];
+var typedWords = [];
 
-input.addEventListener('input', handleInput);
+input.addEventListener("input", handleInput);
 
 /**
  * Function to handle input event
  * @param {InputEvent} e
  */
-function handleInput(e){
-    /**
-     * @type {String}
-     */
-    var inputStr = e.target.value;
-    // Also check if was correct before going to next word
-    if (inputStr.includes(" ")) {
-        typedText.push(inputStr.slice(0, -1))
-        if (compareInput()){
-            console.log("yee");
-            input.value="";
-            document.querySelector(".text-field").innerHTML = typedText;
-        }
-        console.log(typedText)
+function handleInput(e) {
+  /**
+   * @type {String}
+   */
+  var inputStr = e.target.value;
+  // Also check if was correct before going to next word
+  if (inputStr.includes(" ")) {
+    typedWords.push(inputStr.slice(0, -1));
+    if (compareInput()) {
+      input.value = "";
+      displayText();
     }
-
-
-
+    console.log(typedWords);
+  }
 }
-
 
 /**
  * @param {number} value
  */
-function compareInput(){
-    textSaker = code.split(" ");
-    currentWordIndex = (typedText.length -1);
-    textToCompare = typedText[currentWordIndex];
-    
+function compareInput() {
+  textSaker = code.split(" ");
+  currentWordIndex = typedWords.length - 1;
+  textToCompare = typedWords[currentWordIndex];
 
-    console.log(textSaker, textToCompare)
+  console.log(textSaker, textToCompare);
 
-    if (textSaker[currentWordIndex] == textToCompare) {
-        return true;
-    }else {
-        typedText.pop(currentWordIndex);
-        return;
-    }
-
+  if (textSaker[currentWordIndex] == textToCompare) {
+    return true;
+  } else {
+    typedWords.pop(currentWordIndex);
+    return;
+  }
 }
 
-// använder skriver in ord, sedan trycker mellanslag då kollar den om ordet är stämmer och om det gör det gå rdne till nästa ord (suddar ut inputfällt)
+function displayText() {
+  //göra om typedtest till en formatad string, gör en för de man skrivit, en man inte skrivit och en för det man håller på att skriva
+  var typedText = typedWords.join(" ");
 
+  var light = lowlight.highlight("js", typedText);
+  console.log(light.value);
+  var hlTypedText = hastUtilToHtml({ type: "root", children: light.value });
+
+  var remainingText = code.slice(typedText.length); 
+
+  document.querySelector(".text-field").innerHTML = hlTypedText + remainingText;
+}
+
+
+displayText();
+validateWord();
