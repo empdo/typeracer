48db29e98d99273a1405429e135b7bca8100dc54
Emil Essung <emil.essung@live.se>
2020-11-13 10:17:40 +0100

    added crashed page prob not gonna keep it though

--- js/welcomeScript.js
+++ js/welcomeScript.js
@@ -1,24 +1,37 @@
 var welcomeText = 'Hello there human, welcome to..';
 var welcomeObj = document.querySelector('#welcome-text');
+var typeacerObj = document.querySelector('#typeacer-text');
+var time = 0;
+
+function showTypeacer() {
+    setTimeout(welcomeText => {
+        welcomeObj.parentNode.removeChild(welcomeObj);
+        location.replace("/index2.html")
+
+    }, time);
+    time += 100;
+}
 
 window.onload = function () {
-    var time = 0;
+    typeacerObj.style.color = 'transparent';
 
     Array.from(welcomeText).forEach((e, i) => {
         if (i === 18) {
             setTimeout(welcomeText => (welcomeObj.textContent += e), time);
             time += 600;
-        }else if (i == (welcomeText.length -1)) {
-            setTimeout(welcomeText => (welcomeObj.textContent += e) (welcomeObj.id = 'blink-anim'), time);
+        } else if (i == welcomeText.length - 1) {
+            setTimeout(welcomeText => {
+                welcomeObj.textContent += e;
+                welcomeObj.id = 'blink-anim';
+                showTypeacer();
+                //shit i det och gör så att den går till terminalen direkt och säger program crashed
+            }, time);
             time += 100;
-            
-        }else {
+        } else {
             setTimeout(welcomeText => (welcomeObj.textContent += e), time);
             time += 100;
-        } 
+        }
     });
 
-
-
-    //call blinking animation
+    //show typeacer and shake
 };
