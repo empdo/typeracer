f78fb6a9b236805d8c1e2ebb044d189b9d309b5f
Emil Essung <emil.essung@live.se>
2020-11-14 13:23:42 +0100

    added start to templates

--- js/cmdScript.js
+++ js/cmdScript.js
@@ -1,22 +1,24 @@
-"use strict";
-var cmdOutput = document.getElementById("cmd-output-container")
+'use strict';
+var cmdOutput = document.getElementById('cmd-output-container');
 
-
-function handleCmdInput(e){
-    if(e.key === "Enter"){
-        if (e.target.value === "-h" || e.target.value === "--help"){
+function handleCmdInput(e) {
+    if (e.key === 'Enter') {
+        if (e.target.value === '-h' || e.target.value === '--help') {
             Array.from(cmdOutput.childNodes).forEach(e => {
                 cmdOutput.removeChild(e);
-            })
-            
+            });
+            var temp = document.getElementById('help-template');
+            var clon = temp.content.cloneNode(true);
+            cmdOutput.appendChild(clon);
         }
     }
 }
 
 var inputElement = document.querySelector('.cmd-input');
 inputElement.addEventListener('keydown', e => {
-    if (e.key === "Enter"){handleCmdInput(e);}
-    
+    if (e.key === 'Enter') {
+        handleCmdInput(e);
+    }
 });
 inputElement.addEventListener('input', e => {
     handleCmdInput(e);
