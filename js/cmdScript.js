8e0255de2585f48a1d4395224cfe646e063da27e
Emil Essung <emil.essung@live.se>
2020-11-17 22:51:51 +0100

    added cli classes parser

--- js/cmdScript.js
+++ js/cmdScript.js
@@ -2,14 +2,52 @@
 var cmdOutput = document.getElementById('cmd-output-container');
 
 class Command {
+    helpMsg;
     constructor() {}
 
     /** @param {String} command */
-    parse(command) {
-        args = command.split(' ');
+    execute(args) {
+        console.log(args);
+        return "command-not-found-template";
+    }
+}
+
+class HelpCommand extends Command {
+    helpMsg = "The help command";
+
+    execute(args) {
+        if (args.length > 0) {
+            alert(commands[args[0]].helpMsg);
+        }
+        return "help-template";
     }
 
-    /// function handleCmdInput(e) {
+}
+
+class TyperacerCommand extends Command {
+    helpMsg = "Usage: typeracer <language>"
+
+    execute(args) {
+        return "typeracer-template";
+    }
+}
+
+const commands = {
+    help: new HelpCommand(),
+    typeracer: new TyperacerCommand()
+}
+
+const defaultCommand = new Command();
+
+function parseCommand(command) {
+    const args = command.split(" ");
+    const mainArg = args[0];
+    var commandObj = defaultCommand;
+    if (mainArg in commands) {
+        commandObj = commands[mainArg]
+    }
+    const templateName = commandObj.execute(args.slice(1));
+    changeCmdOutput(templateName);
 }
 
 function changeCmdOutput(templateName) {
@@ -22,7 +60,8 @@ function changeCmdOutput(templateName) {
 }
 
 var inputElement = document.querySelector('.cmd-input');
-
-inputElement.addEventListener('input', e => {
-    handleCmdInput(e);
+var inputForm = document.querySelector(".cmd-form");
+inputForm.addEventListener('submit', e => {
+    e.preventDefault()
+    parseCommand(inputElement.value)
 });
