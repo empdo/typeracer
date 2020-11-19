f06ccdc1ae3fe67a9aa8c445e9a529e38a83f9f7
empdo <emil.essung@live.se>
2020-11-19 18:12:49 +0100

    start of bottom bar

--- js/textEditor.js
+++ js/textEditor.js
@@ -2,13 +2,14 @@
 
 var sideBar = document.getElementById('side-bar');
 
-for (var i = 1; i < 100 ; i++) {
+for (var i = 1; i < 41 ; i++) {
     var number = document.createElement('span');
-    number.className = ('side-bar-number')
     if (i < 12 /*number of rows of code */){
         number.textContent = i;
+        number.className = ('side-bar-number')
     }else {
         number.textContent = "~";
+        number.className = ('side-bar-symbol')
     }
     
     sideBar.appendChild(number);
