bce7e99ca4412753557fd1cd973b8ef1c49af0f6
Emil Essung <emil.essung@live.se>
2020-12-14 17:04:20 +0100

    yes

--- js/githubAuth.js
+++ js/githubAuth.js
@@ -2,6 +2,7 @@
 var key = "9b3060d8b4ddf1f2a7b8";
 //window.location.replace(`https://github.com/login/oauth/authorize?client_id=${key}`); 
 var code = new URLSearchParams(window.location.search).get("code")
+console.log(code)
 
 userToken(code).then(data => {
 	window.localStorage.setItem("token", data);
